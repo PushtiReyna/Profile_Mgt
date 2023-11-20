6d113f72230a0c4b1e63f252b8583db7305d0e2d
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-11-20 14:15:15 +0530

    PP - Create project

---
+++ Profile Mgt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Profile_Mgt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
